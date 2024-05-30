9086c6b923728a605061ad2114f31a84c1c7209f
achrefmechergui <achreffcb123achref@gamil.com>
2024-05-30 16:31:30 +0100

    delete-update

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const express = require('express')
 const app = express()
 const db = require('./database.js')
 const foodroutes = require('./routes/foodroutes.js')
+const cors = require('cors')
+const path = require('path')
 
+
+app.use(cors())
+
+
+
+app.use(express.static(path.join(__dirname,'View')))
 
 app.use(express.json()) 
 app.use('/',foodroutes)
+
+
 
 
 app.listen(5000,()=>{
